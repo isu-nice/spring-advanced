8e9274e70d04d4a6d6b18a5ceb738b7d8c9ad0c3
isu-nice <isu010614@naver.com>
2024-02-24 11:02:43 +0900

    feat: 템플릿 메서드 테스트

---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,22 @@
+package hello.advanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행 //
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료 //
+        System.currentTimeMillis();
+    }
+
+}
