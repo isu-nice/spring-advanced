2da3311fb5dd146b33d088416d4dfffc2f945b0d
isu-nice <isu010614@naver.com>
2024-02-27 17:13:22 +0900

    feat: 비즈니스 로직2(테스트용)

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -9,6 +9,7 @@ public class TemplateMethodTest {
     @Test
     void templateMethodV0() {
         logic1();
+        logic2();
     }
 
     private void logic1() {
@@ -16,7 +17,18 @@ public class TemplateMethodTest {
         // 비즈니스 로직 실행 //
         log.info("비즈니스 로직1 실행");
         // 비즈니스 로직 종료 //
-        System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
     }
 
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행 //
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료 //
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
 }
