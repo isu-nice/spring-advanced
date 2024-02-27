5c3d16e5a50b14f33b71aa552a6a4b1907383ca4
isu-nice <isu010614@naver.com>
2024-02-27 17:26:57 +0900

    test: 템플릿 메서드 패턴 적용 테스트

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,11 +1,18 @@
 package hello.advanced.trace.template;
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class TemplateMethodTest {
 
+    /**
+     * 템플릿 메서드 패턴 적용 X
+     * 유사한 로직을 두 번 실행
+     */
     @Test
     void templateMethodV0() {
         logic1();
@@ -30,5 +37,17 @@ public class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1(); //SubClassLogic1 인스턴스
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 }
