5041d4b0ffe382e8df69f76699983052362b2397
isu-nice <isu010614@naver.com>
2024-02-28 04:22:23 +0900

    feat: 내부 클래스 로직 2

---
+++ src/test/java/hello/advanced/trace/template/code/SubClassLogic2.java
@@ -0,0 +1,14 @@
+package hello.advanced.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 템플릿 메서드 패턴 적용
+ */
+@Slf4j
+public class SubClassLogic2 extends AbstractTemplate{
+    @Override
+    protected void call() {
+        log.info("비즈니스 로직2 실행");
+    }
+}
