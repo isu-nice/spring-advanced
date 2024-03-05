c46d385420baf55c0994ee055368733c57aeb478
isu-nice <isu010614@naver.com>
2024-03-05 17:46:17 +0900

    chore: 컨벤션 정리

--- src/test/java/hello/advanced/trace/template/code/SubClassLogic2.java
+++ src/test/java/hello/advanced/trace/template/code/SubClassLogic2.java
@@ -6,7 +6,8 @@ import lombok.extern.slf4j.Slf4j;
  * 템플릿 메서드 패턴 적용
  */
 @Slf4j
-public class SubClassLogic2 extends AbstractTemplate{
+public class SubClassLogic2 extends AbstractTemplate {
+
     @Override
     protected void call() {
         log.info("비즈니스 로직2 실행");
